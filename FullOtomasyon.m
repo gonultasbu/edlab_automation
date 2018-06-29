4d07a27e90d545a4dc307a57424dd7d16e93b8b9
Burak Mert Gonultas <gonultasbu@users.noreply.github.com>
2018-06-29 11:08:44 +0300

    commentation

--- FullOtomasyon.m
+++ FullOtomasyon.m
@@ -1,8 +1,10 @@
 clc
-clearvars -except maks MI b
+clearvars -except maks MI b %Since the script is iterated multiple times, some variables must be preserved
 
+%Find the serial object on COM6 serial port, define the timestamp
 obj1 = instrfind('Type', 'serial', 'Port', 'COM6', 'Tag', '');
 time=datestr(now,'mm-dd-yyyy HH'); %SHOULD AVOID RUNNING IT AT HH:00
+%Open the text file for storing the results
 fid=fopen(time,'a');
 if isempty(obj1)
     obj1 = serial('COM6')
@@ -13,15 +15,16 @@ end
 fclose(obj1);
 fopen(obj1);
 
-% READ THE CODE BELOW FROM THE DOCUMENTATION, DO NOT MESS WITH IT EXCEPT FOR cam.Timing.Exposure.Set()
 
+%Import the DLL for accessing the ThorCam software, define the camera, this part is read from the ThorCam Documentation.
 NET.addAssembly('C:\Program Files\Thorlabs\Scientific Imaging\DCx Camera Support\Develop\DotNet\uc480dotNet.dll');
 cam=uc480.Camera;
 cam.Init(1);
 cam.Display.Mode.Set(uc480.Defines.DisplayMode.DiB);
 cam.PixelFormat.Set(uc480.Defines.ColorMode.RGBA8Packed);
 cam.Trigger.Set(uc480.Defines.TriggerMode.Software);
-cam.Timing.Exposure.Set(8)             %HARD THRESHOLD ALERT
+%Define the Exposure, this is the only modifiable parameter in this section.
+cam.Timing.Exposure.Set(8)            
 [~,MemId] = cam.Memory.Allocate(true);
 [~,Width,Height,Bits,~]=cam.Memory.Inquire(MemId);
 cam.Acquisition.Freeze(uc480.Defines.DeviceParameter.Wait);
@@ -29,30 +32,32 @@ cam.Acquisition.Freeze(uc480.Defines.DeviceParameter.Wait);
 Data= reshape(uint8(tmp), [Bits/8, Width, Height]);
 Data = Data(1:3 , 1:Width, 1:Height);
 Data=permute(Data, [3,2,1]);
+%Uncomment imtool if you wish to check the snapshots and debug.
 %imtool(Data);
 cam.Exit;
 
-%THE IMAGE IS TAKEN AS A MATRIX IN Data
 
-%TO FIND THE MIDDLE POINT OF THE SCANLINE, PIXEL LOCATIONS WITH VALUES ABOVE 10 ARE FOUND
+
+%Snapshot is taken as "Data" variable.
+%Weak noise filter.
 [rowtest, coltest]=find(Data>10);
-%THE LINE CUTTING THROUGH THE SCANLINE HORIZONTALLY IS ENOUGH, THE LINE IS VERTICAL ANYWAY
+%Find the middle point of the scanline.
 ROW_MIDDLE=mean(rowtest);
 
-%FIND THE BRIGHTEST SET OF POINTS ABOVE THE MIDDLE LINE, CONSIDER THAT THE INDICES AND THE ACTUAL IMAGE IS REVERSED
+%Find the brightest set of points above the middle line, consider that the indices and the actual image are reversed. (y,x)
 mtop=max(max(Data(1:round(ROW_MIDDLE),1:1200)));
-[rowtop, coltop]=find(Data>mtop-50 & Data<mtop+1); %HARD PARAMETER OF 50
+[rowtop, coltop]=find(Data>mtop-50 & Data<mtop+1); 
 rowtop=rowtop(rowtop<ceil(ROW_MIDDLE)+1 & rowtop>1);
 coltop=coltop(coltop<1200 & coltop>1);
 
 
-%FIND THE BRIGHTEST SET OF POINTS BELOW THE MIDDLE LINE, CONSIDER THAT THE INDICES AND THE ACTUAL IMAGE IS REVERSED
+%Find the brightest set of points below the middle line, consider that the indices and the actual image are reversed. (y,x)
 mbot=max(max(Data(round(ROW_MIDDLE):1024,1:1200)));
 [rowbot, colbot]=find(Data>mbot-50 & Data<mbot+1); %HARD PARAMETER OF 50 
 rowbot=rowbot(rowbot<1024 & rowbot>floor(ROW_MIDDLE)-1);
 colbot=colbot(colbot<1200 & colbot>1);
 
-
+%Standard distance calculation.
 distance=sqrt((max(rowtop)-min(rowbot)).^2 + (max(coltop)-min(colbot)).^2)
 fprintf(fid,num2str(distance));
 fprintf(fid,'\n');
