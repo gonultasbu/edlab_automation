df890ab58688d944b1e2b7dc970e3cf97e481298
Burak Mert Gonultas <gonultasbu@users.noreply.github.com>
2018-06-29 11:14:28 +0300

    commentation

--- ForLoop.m
+++ ForLoop.m
@@ -1,4 +1,6 @@
 %THIS FILE SWEEPS THE FREQUENCY AND CALLS THE DETECTION ALGORITHM
+
+%+-20 Hz Range
 for b= maks-20:1:maks+20
     obj1 = instrfind('Type', 'serial', 'Port', 'COM6', 'Tag', '');
     if isempty(obj1)
@@ -8,18 +10,23 @@ for b= maks-20:1:maks+20
       obj1 = obj1(1);
 end
 
+%Frequency data sent through serial port.
 h=strcat('WMF0000',num2str(b),'00+0x0a');
 
 fclose(obj1);
 fopen(obj1);
+%Waveform data sent through serial port. (Sine Wave here)
 fprintf(obj1, 'WMW00+ 0x0a');
+%Amplitude data through serial port. (17 V Here)
 fprintf(obj1, 'WMA17.0+ 0x0a');
 fprintf(obj1, h);
+%Call the core function. (40 times in total)
 FullOtomasyon
 fclose(obj1);
 delete(obj1);
 end;
 
+%Find the resonant frequency and match the generator frequency.
 time=datestr(now,'mm-dd-yyyy HH');
 C=textread(time,'%f');
 MI=find(C == max(C(:)));
